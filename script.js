3fb76c7d9cbc10b2533ae8c417b06b2e16c27776
birechdebbie <birechdebora@gmail.com>
2025-05-12 07:00:14 +0300

    finished assignment

--- script.js
+++ script.js
@@ -89,20 +89,4 @@ const slides = [
     updateSlideshow();
   }, 5000); // Change image every 5 seconds
 
-
- /*  let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.display = i === index ? 'block' : 'none';
-  });
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-setInterval(nextSlide, 3000); // Change every 3 seconds
  */
